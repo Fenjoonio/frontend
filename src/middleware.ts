142121f8f8b19c79e4ead28637cdd8c3226dbe23
Yasin Silavi <ys.silavi@gmail.com>
2025-02-25 17:32:57 +0330

    feat: add redirect middleware

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+function handleOnboarding(request: NextRequest): NextResponse | undefined {
+  if (request.cookies.has("isFirstTime")) return;
+
+  const expireDate = new Date();
+  expireDate.setFullYear(expireDate.getFullYear() + 1);
+
+  return NextResponse.redirect(new URL("/onboarding", request.url), {
+    headers: {
+      "Set-Cookie": `${"isFirstTime"}=false; expires=${expireDate.toUTCString()}; path=/`,
+    },
+  });
+}
+
+function handleAuthentication(request: NextRequest): NextResponse | undefined {
+  const isLoggedIn = request.cookies.has("accessToken");
+  const isFirstRun = !request.cookies.has("isFirstTime");
+  const { pathname } = request.nextUrl;
+
+  if (!isLoggedIn && !isFirstRun && !pathname.includes("/accounts")) {
+    const params = new URLSearchParams();
+    params.set("redirect", pathname);
+
+    return NextResponse.redirect(new URL(`/accounts/login?${params.toString()}`, request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export function middleware(request: NextRequest) {
+  return handleOnboarding(request) || handleAuthentication(request) || NextResponse.next();
+}
+
+export const config = { matcher: "/((?!api|static|.*\\..*|_next).*)" };
