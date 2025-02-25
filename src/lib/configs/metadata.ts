2de75654e186f2f02cf2ab8afc550a60f9ac603c
Yasin Silavi <ys.silavi@gmail.com>
2025-02-25 13:21:44 +0330

    feat: add home page metadata

---
+++ src/lib/configs/metadata.ts
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "فنجون؛ یک فنجان خواندنی",
+    template: "فنجون",
+  },
+  description: "یک دنیا داستان‌های خواندنی کوتاه",
+};
