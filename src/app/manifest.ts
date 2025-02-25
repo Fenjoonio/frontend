15872bdf6da4a55768fa21fee83d01c19245a1f9
Yasin Silavi <ys.silavi@gmail.com>
2025-02-26 01:30:20 +0330

    feat: add manifest file and icons

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "فنجون",
+    short_name: "Fenjoon",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#2e2e2e",
+    theme_color: "#e0e0e0",
+    icons: [
+      {
+        src: "/manifest-assets/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/manifest-assets/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
