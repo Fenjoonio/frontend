b2ef25719e7c966372483f7457251a0d58c03ac6
Yasin Silavi <ys.silavi@gmail.com>
2025-02-25 13:15:52 +0330

    feat: add aria fonts

--- src/lib/configs/fonts.ts
+++ src/lib/configs/fonts.ts
@@ -1,47 +1,102 @@
 import localFont from "next/font/local";
 
-export const yekanBakh = localFont({
+export const aria = localFont({
   display: "swap",
-  variable: "--font-yekan-bakh",
+  variable: "--font-aria",
   src: [
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Thin.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Thin.woff2",
       weight: "100",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Light.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-ExtraLight.woff2",
       weight: "200",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Light.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Light.woff2",
       weight: "300",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Regular.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Normal.woff2",
       weight: "400",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-SemiBold.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Regular.woff2",
+      weight: "400",
+    },
+    {
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-SemiBold.woff2",
       weight: "600",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Bold.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Bold.woff2",
       weight: "700",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-ExtraBold.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-UltraBold.woff2",
       weight: "800",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Black.woff2",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-ExtraBold.woff2",
+      weight: "800",
+    },
+    {
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Black.woff2",
       weight: "900",
     },
     {
-      path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-ExtraBlack.woff2",
-      weight: "950",
+      path: "../../../public/assets/fonts/aria/AriaWebFaNum-Heavy.woff2",
+      weight: "900",
     },
   ],
 });
+
+// export const yekanBakh = localFont({
+//   display: "swap",
+//   variable: "--font-yekan-bakh",
+//   src: [
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Thin.woff2",
+//       weight: "100",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Light.woff2",
+//       weight: "200",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Light.woff2",
+//       weight: "300",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Regular.woff2",
+//       weight: "400",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-SemiBold.woff2",
+//       weight: "600",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Bold.woff2",
+//       weight: "700",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-ExtraBold.woff2",
+//       weight: "800",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-Black.woff2",
+//       weight: "900",
+//     },
+//     {
+//       path: "../../../public/assets/fonts/yekan-bakh/YekanBakhFaNum-ExtraBlack.woff2",
+//       weight: "950",
+//     },
+//   ],
+// });
 
 export const rokh = localFont({
   display: "swap",
