adabd7782c49758256075c5ba0e3d06a8334c0ee
Yasin Silavi <ys.silavi@gmail.com>
2025-02-25 17:32:16 +0330

    feat: implement onboarding page

---
+++ src/app/(user)/onboarding/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function OnboardingPage() {
+  return (
+    <section className="h-svh flex flex-col items-center justify-center px-4">
+      <h1 className="text-2xl font-bold">به فنجون خوش اومدی!</h1>
+      <p className="text-[#B0B0B0] text-center mt-2">
+        اینجا میتونی کلی داستان کوتاه بخونی و بنویسی و لذت ببری!
+      </p>
+
+      <Link href="/">
+        <Button className="w-40 mt-16">بزن بریم!</Button>
+      </Link>
+    </section>
+  );
+}
