0d7bc29344335541cc1df667d63395c19f8ea82e
Yasin Silavi <ys.silavi@gmail.com>
2025-02-26 12:55:23 +0330

    feat: update loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,11 @@
 export default function Loading() {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center fixed top-0 right-0 bg-white z-20">
-      <h1 className="text-2xl text-[#D4AF37] font-rokh font-bold">فنجون</h1>
-      <span className="text-[#3D3D4E] mt-2">یک فنجان خواندنی</span>
+    <div className="w-full h-full flex flex-col items-center justify-end fixed top-0 right-0 bg-[#2e2e2e] z-20 pb-20">
+      <div className="w-[480px] mx-auto text-center relative mb-auto mt-auto select-none">
+        <h1 className="text-7xl font-bold">فــــ</h1>
+      </div>
+
+      <span className="text-sm text-[#B0B0B0] mt-2">در حال بارگذاری ...</span>
     </div>
   );
 }
