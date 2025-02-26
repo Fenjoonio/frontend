ecde5223994fae8514758fba8c57fa3157dc606d
Yasin Silavi <ys.silavi@gmail.com>
2025-02-26 16:59:00 +0330

    feat: update manifest file

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,13 +2,21 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "فنجون",
+    id: "/",
+    dir: "rtl",
+    lang: "fa",
+    name: "Fenjoon",
     short_name: "Fenjoon",
-    description: "",
+    description: `Fenjoon is a PWA for reading and writing short stories. Fast, lightweight, and always accessible`,
+    categories: ["books", "entertainment", "lifestyle", "writing", "news", "education"],
     start_url: "/",
     display: "standalone",
     background_color: "#2e2e2e",
     theme_color: "#e0e0e0",
+    orientation: "portrait",
+    launch_handler: {
+      client_mode: "auto",
+    },
     icons: [
       {
         src: "/manifest-assets/icons/icon-192x192.png",
