8f551253f17d1301bfbdc16353d67b6b2e170a50
Yasin Silavi <ys.silavi@gmail.com>
2025-02-26 20:31:59 +0330

    feat: handle offline mode

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,9 @@ const nextConfig: NextConfig = {};
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
+  cacheOnNavigation: true,
   disable: process.env.NODE_ENV !== "production",
+  additionalPrecacheEntries: [{ url: "/~offline", revision: crypto.randomUUID() }],
 });
 
 export default withSerwist(nextConfig);
