711cf80b3ab400c1c9606399eaa12d9b73e347d8
Yasin Silavi <ys.silavi@gmail.com>
2025-02-26 13:03:56 +0330

    fix: update logo on loading page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,11 @@
 export default function Loading() {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-end fixed top-0 right-0 bg-[#2e2e2e] z-20 pb-20">
-      <div className="w-[480px] mx-auto text-center relative mb-auto mt-auto select-none">
-        <h1 className="text-7xl font-bold">فــــ</h1>
+    <div className="w-full h-full flex flex-col items-center justify-center fixed top-0 right-0 bg-[#2e2e2e] z-20 pb-20">
+      <div className="w-full text-center select-none mt-auto">
+        <h1 className="text-8xl font-extrabold">فــــ</h1>
       </div>
 
-      <span className="text-sm text-[#B0B0B0] mt-2">در حال بارگذاری ...</span>
+      <span className="text-sm text-[#B0B0B0] mt-auto">در حال بارگذاری ...</span>
     </div>
   );
 }
