f37d04293bfd26c63d04b3e70a9fc024dd3cf397
Yasin Silavi <ys.silavi@gmail.com>
2025-03-01 05:32:38 +0330

    fix: change pwa display mode to fullscreen

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,7 +12,7 @@ export default function manifest(): MetadataRoute.Manifest {
     description: `Fenjoon is a PWA for reading and writing short stories. Fast, lightweight, and always accessible`,
     categories: ["books", "entertainment", "lifestyle", "writing", "news", "education"],
     start_url: "/",
-    display: "standalone",
+    display: "fullscreen",
     background_color: "#2e2e2e",
     theme_color: "#e0e0e0",
     orientation: "portrait",
