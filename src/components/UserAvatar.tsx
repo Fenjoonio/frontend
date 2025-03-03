831e72e55afed87d8a9373cede097497d71cee8c
Yasin Silavi <ys.silavi@gmail.com>
2025-03-03 18:38:15 +0330

    feat: don't let user avatars shrink

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -12,7 +12,10 @@ export default function UserAvatar({ user, className }: UserAvatarProps) {
 
   return (
     <div
-      className={cn("w-7 h-7 flex items-center justify-center rounded-lg bg-[#9C6B4A]", className)}
+      className={cn(
+        "w-7 h-7 shrink-0 flex items-center justify-center rounded-lg bg-[#9C6B4A]",
+        className
+      )}
     >
       <div className="w-4 h-4 flex justify-center items-center overflow-hidden text-sm font-bold">
         {userName[0]}
