faa69cac2c25b6d934e467a71ed5772a895e74f4
Yasin Silavi <ys.silavi@gmail.com>
2025-03-06 03:25:49 +0330

    fix: change show loading condition

--- src/providers/LoadingProvider.tsx
+++ src/providers/LoadingProvider.tsx
@@ -5,7 +5,7 @@ import { isDev, isApp } from "@/lib/utils/environment";
 import { type PropsWithChildren, useLayoutEffect, useState } from "react";
 
 export default function LoadingProvider({ children }: PropsWithChildren) {
-  const [isLoading, setIsLoading] = useState(!isDev() && !isApp());
+  const [isLoading, setIsLoading] = useState(!(isDev() || isApp()));
 
   useLayoutEffect(() => {
     const timeout = setTimeout(() => setIsLoading(false), 1500);
