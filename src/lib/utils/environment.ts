0fb970affe7b27b0828f9daa1ec79801f2acb67c
Yasin Silavi <ys.silavi@gmail.com>
2025-03-06 03:21:16 +0330

    feat: add is-app environment

--- src/lib/utils/environment.ts
+++ src/lib/utils/environment.ts
@@ -5,3 +5,7 @@ export function isDev() {
 export function isClientSide() {
   return typeof window !== "undefined";
 }
+
+export function isApp() {
+  return isClientSide() && navigator.userAgent === "Fenjoon-WebView";
+}
