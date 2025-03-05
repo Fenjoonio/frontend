c3bf0f9c00524ea3455b9aa6db82737e7fdcca77
Yasin Silavi <ys.silavi@gmail.com>
2025-03-06 03:21:31 +0330

    feat: disable loading in app

--- src/providers/LoadingProvider.tsx
+++ src/providers/LoadingProvider.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import Loading from "@/components/Loading";
-import { isDev } from "@/lib/utils/environment";
+import { isDev, isApp } from "@/lib/utils/environment";
 import { type PropsWithChildren, useLayoutEffect, useState } from "react";
 
 export default function LoadingProvider({ children }: PropsWithChildren) {
-  const [isLoading, setIsLoading] = useState(!isDev());
+  const [isLoading, setIsLoading] = useState(!isDev() && !isApp());
 
   useLayoutEffect(() => {
     const timeout = setTimeout(() => setIsLoading(false), 1500);
