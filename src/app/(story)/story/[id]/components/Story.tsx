d6bb95939349e5ee779db6bc7884e699f4df4a34
Yasin Silavi <ys.silavi@gmail.com>
2025-03-12 18:33:48 +0330

    feat: add share button inside story page

--- src/app/(story)/story/[id]/components/Story.tsx
+++ src/app/(story)/story/[id]/components/Story.tsx
@@ -7,6 +7,8 @@ import { getUserName } from "@/lib/utils/users";
 import { useGetSingleStory } from "@/services/stories";
 import { formatStoryCreateAt } from "@/lib/utils/story";
 import StoryLikeButton from "@/app/(story)/components/StoryLikeButton";
+import ShareStorySheet from "@/app/(story)/components/ShareStorySheet";
+import { Share2Icon } from "lucide-react";
 
 type StoryProps = {
   id: string;
@@ -14,6 +16,7 @@ type StoryProps = {
 
 export default function Story({ id }: StoryProps) {
   const [showFullText, setShowFullText] = useState(false);
+  const [isShareSheetOpen, setIsShareSheetOpen] = useState(false);
   const { data: story, isPending, isFetched } = useGetSingleStory({ id: +id });
   const hasStory = story && Object.keys(story).length;
 
@@ -58,14 +61,32 @@ export default function Story({ id }: StoryProps) {
         </span>
       )}
 
-      <div className="flex items-center gap-x-2 mt-8">
-        <StoryLikeButton
-          storyId={story.id}
-          isLikedByUser={story.isLikedByUser}
-          className="w-5 h-5 text-[#B0B0B0]"
-        />
-        <span className="text-sm text-[#B0B0B0]">{story.likesCount}</span>
+      <div className="flex items-center gap-x-4 mt-8">
+        <div className="flex items-center gap-x-2">
+          <StoryLikeButton
+            storyId={story.id}
+            isLikedByUser={story.isLikedByUser}
+            className="w-5 h-5 text-[#B0B0B0]"
+          />
+          <span className="text-sm text-[#B0B0B0]">{story.likesCount}</span>
+        </div>
+
+        <div className="flex items-center gap-x-2">
+          <Share2Icon
+            className="w-5 h-5 text-[#B0B0B0] ms-auto"
+            onClick={() => setIsShareSheetOpen(true)}
+          />
+          <span className="text-sm text-[#B0B0B0]">{story.sharesCount}</span>
+        </div>
       </div>
+
+      {isShareSheetOpen && (
+        <ShareStorySheet
+          storyId={story.id}
+          isOpen={isShareSheetOpen}
+          onOpenChange={setIsShareSheetOpen}
+        />
+      )}
     </div>
   );
 }
