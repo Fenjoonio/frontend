5c83dbd857de5c4c101569f814f9f7a18cd907a8
Yasin Silavi <ys.silavi@gmail.com>
2025-03-16 05:46:37 +0330

    feat: don't show loading on app

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,20 +1,18 @@
-import { cookies } from "next/headers";
 import GTMProvider from "./GTMProvider";
 import AuthProvider from "./AuthProvider";
 import QueryProvider from "./QueryProvider";
 import LoadingProvider from "./LoadingProvider";
 import { ToastContainer } from "react-toastify";
-import { isDev } from "@/lib/utils/environment";
 import { PropsWithChildren, Suspense } from "react";
+import { isApp, isDev } from "@/lib/utils/environment";
 import { getUserCredentials } from "@/app/(user)/accounts/actions";
 
 export default async function Providers({ children }: PropsWithChildren) {
-  const { get } = await cookies();
   const { accessToken } = await getUserCredentials();
 
   return (
     <Suspense>
-      <LoadingProvider initialValue={!isDev() && get("isWebView")?.value !== "true"}>
+      <LoadingProvider initialValue={!(isDev() || isApp())}>
         <QueryProvider>
           <AuthProvider initialAccessToken={accessToken}>
             <GTMProvider>{children}</GTMProvider>
