dfc3af26c0d661a4270ecef7719df7c3e273eeaa
Yasin Silavi <ys.silavi@gmail.com>
2025-03-19 15:28:28 +0330

    refactor: update push token logic

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -2,17 +2,31 @@
 
 import { isApp } from "@/lib/utils/environment";
 import { registerPushToken } from "@/services/push";
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect, useRef } from "react";
 
 export default function AppProvider({ children }: PropsWithChildren) {
+  const timer = useRef<NodeJS.Timeout | undefined>(undefined);
+
   useEffect(() => {
     if (!isApp()) return;
 
-    // This Function will call on the app
-    (window as any).setUserPushToken = async (token: string) => {
-      await registerPushToken({ token });
-      alert(token);
+    const getPushToken = () => {
+      const pushToken = window.expoPushToken;
+
+      alert("I'm called " + pushToken);
+      if (pushToken) {
+        registerPushToken({ token: pushToken }).catch((err) => {
+          alert("I'm error " + JSON.stringify(err));
+        });
+        clearTimeout(timer.current);
+
+        return;
+      }
+
+      timer.current = setTimeout(getPushToken, 3000);
     };
+
+    getPushToken();
   }, []);
 
   return children;
