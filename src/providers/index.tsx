92c99f6ddb1a27c811e53941613eba1e14019c93
Yasin Silavi <ys.silavi@gmail.com>
2025-03-19 16:37:03 +0330

    fix: disable loader on app

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,3 +1,5 @@
+import { headers } from "next/headers";
+import { userAgent } from "next/server";
 import AppProvider from "./AppProvider";
 import GTMProvider from "./GTMProvider";
 import AuthProvider from "./AuthProvider";
@@ -5,24 +7,24 @@ import QueryProvider from "./QueryProvider";
 import LoadingProvider from "./LoadingProvider";
 import { ToastContainer } from "react-toastify";
 import { PropsWithChildren, Suspense } from "react";
-import { isApp, isDev } from "@/lib/utils/environment";
 import { getUserCredentials } from "@/app/(user)/accounts/actions";
 
 export default async function Providers({ children }: PropsWithChildren) {
   const { accessToken } = await getUserCredentials();
+  const { ua } = userAgent({ headers: await headers() });
 
   return (
     <Suspense>
-      <LoadingProvider initialValue={!(isDev() || isApp())}>
-        <AppProvider>
-          <QueryProvider>
-            <AuthProvider initialAccessToken={accessToken}>
-              <GTMProvider>{children}</GTMProvider>
-            </AuthProvider>
+      <LoadingProvider initialValue={ua !== "Fenjoon-WebView"}>
+        <QueryProvider>
+          <AuthProvider initialAccessToken={accessToken}>
+            <GTMProvider>
+              <AppProvider>{children}</AppProvider>
+            </GTMProvider>
+          </AuthProvider>
 
-            <ToastContainer rtl draggable limit={2} draggablePercent={10} />
-          </QueryProvider>
-        </AppProvider>
+          <ToastContainer rtl draggable limit={2} draggablePercent={10} />
+        </QueryProvider>
       </LoadingProvider>
     </Suspense>
   );
