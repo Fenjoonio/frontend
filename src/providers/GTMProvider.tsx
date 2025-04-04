df9c7d637b8dabd35573b140607fc259dd60c569
Yasin Silavi <ys.silavi@gmail.com>
2025-04-05 01:33:52 +0330

    feat: add tag manager provider

--- src/providers/GTMProvider.tsx
+++ src/providers/GTMProvider.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from "react";
-// import { isDev } from "@/lib/utils/environment";
-// import { GoogleTagManager } from "@next/third-parties/google";
+import { isDev } from "@/lib/utils/environment";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function GTMProvider({ children }: PropsWithChildren) {
   return (
     <>
       {children}
-      {/* {!isDev() && <GoogleTagManager gtmId="GTM-WFZ5Z9X4" />} */}
+      {!isDev() && <GoogleTagManager gtmId="GTM-PSXG6RHK" />}
     </>
   );
 }
