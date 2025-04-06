7322d3404565bfdfdafb55f4176ec43be379629d
Yasin Silavi <ys.silavi@gmail.com>
2025-04-06 03:41:31 +0330

    feat: update textarea component styles

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,12 +7,12 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "w-full min-h-16 field-sizing-content border border-border placeholder:text-gray-400 dark:placeholder:text-gray-300 focus-visible:border-foreground aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 rounded-md bg-transparent px-3 py-2 transition-colors outline-none disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
     />
-  )
+  );
 }
 
 export { Textarea }
