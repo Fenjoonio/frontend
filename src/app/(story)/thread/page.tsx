190a6b4edc8cc540681cd8d9886665a565fb7425
Yasin Silavi <ys.silavi@gmail.com>
2025-04-12 15:22:59 +0330

    feat: update thread story

--- src/app/(story)/thread/page.tsx
+++ src/app/(story)/thread/page.tsx
@@ -17,7 +17,7 @@ import ShareStorySheet from "@/app/(story)/components/ShareStorySheet";
 import { MessageSquareTextIcon, PenIcon, Share2Icon } from "lucide-react";
 import { useGetSingleStory, useGetInfiniteStoryComments } from "@/services/stories";
 
-const STORY_ID = 99;
+const STORY_ID = 122;
 
 // TODO: SSR this page (get first story server side)
 export default function ThreadPage() {
