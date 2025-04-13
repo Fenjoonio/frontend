b42e0cb9b79c2085d558b6e773c1113c8c976d90
Yasin Silavi <ys.silavi@gmail.com>
2025-04-14 01:13:06 +0330

    feat: use infinite pagination to load all the user comments

--- src/app/(user)/author/[id]/components/UserComments.tsx
+++ src/app/(user)/author/[id]/components/UserComments.tsx
@@ -3,25 +3,29 @@
 import { useMemo } from "react";
 import { Comment, CommentSkeleton } from "@/components/Comment";
 import { useGetUserCommentsById } from "@/services/user";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 type UserCommentsProps = {
   id: number;
 };
 
 export default function UserComments({ id }: UserCommentsProps) {
-  const { data, isFetching } = useGetUserCommentsById({ id });
+  const { data, isFetching, fetchNextPage, hasNextPage } = useGetUserCommentsById({
+    id,
+    page: 1,
+    limit: 10,
+  });
 
   const comments = useMemo(() => {
     return data?.pages ? data.pages.flatMap((page) => page.comments ?? []) : [];
   }, [data?.pages]);
 
   return (
-    <section className="divide-y divide-border">
-      {comments.map((comment) => (
-        <Comment key={comment.id} comment={comment} className="pb-6 not-first:pt-6" />
-      ))}
-
-      {isFetching && (
+    <InfiniteScroll
+      next={fetchNextPage}
+      dataLength={comments.length}
+      hasMore={isFetching || hasNextPage}
+      loader={
         <>
           {Array(5)
             .fill(0)
@@ -29,7 +33,12 @@ export default function UserComments({ id }: UserCommentsProps) {
               <CommentSkeleton key={index} className="flex gap-x-2 pb-6 not-first:pt-6" />
             ))}
         </>
-      )}
-    </section>
+      }
+      className="divide-y divide-border"
+    >
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} className="pb-6 not-first:pt-6" />
+      ))}
+    </InfiniteScroll>
   );
 }
