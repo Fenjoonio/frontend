0afc2c05a47395840214d833b7aff119f60ca277
Yasin Silavi <ys.silavi@gmail.com>
2025-04-14 01:12:59 +0330

    feat: use infinite pagination to load all the user stories

--- src/app/(user)/author/[id]/components/UserStories.tsx
+++ src/app/(user)/author/[id]/components/UserStories.tsx
@@ -7,6 +7,7 @@ import { getUserName } from "@/lib/utils/users";
 import UserAvatar from "@/components/UserAvatar";
 import { formatStoryCreateAt } from "@/lib/utils/story";
 import { useGetUserStoriesById } from "@/services/user";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { MessageSquareTextIcon, Share2Icon } from "lucide-react";
 import ShareStorySheet from "@/app/(story)/components/ShareStorySheet";
 import StoryLikeButton from "@/app/(story)/components/StoryLikeButton";
@@ -17,8 +18,8 @@ type UserStoriesProps = {
 
 export default function UserStories({ id }: UserStoriesProps) {
   const [sharedStoryId, setSharedStoryId] = useState(0);
-  const { data, isFetching } = useGetUserStoriesById({ id });
   const [isShareSheetOpen, setIsShareSheetOpen] = useState(false);
+  const { data, isFetching, fetchNextPage, hasNextPage } = useGetUserStoriesById({ id });
 
   const stories = useMemo<Story[]>(() => {
     return data?.pages ? data.pages.flatMap((page) => page.stories ?? []) : [];
@@ -31,51 +32,11 @@ export default function UserStories({ id }: UserStoriesProps) {
 
   return (
     <>
-      <section className="divide-y divide-border">
-        {stories.map((story) => (
-          <div key={story.id} className="flex gap-x-2 pb-6 not-first:pt-6">
-            <UserAvatar user={story.user} />
-
-            <div className="flex-1 mt-1">
-              <Link href={`/story/${story.id}`}>
-                <div className="flex gap-x-2 items-center">
-                  <span className="font-bold">{getUserName(story.user)}</span>
-                  <span className="size-1 bg-gray-300 dark:bg-border rounded-sm"></span>
-                  <span className="text-[10px] text-light-gray-900 dark:text-soft-foreground">
-                    {formatStoryCreateAt(story.createdAt)}
-                  </span>
-                </div>
-
-                <p className="w-full text-sm text-soft-foreground whitespace-pre-line line-clamp-6 leading-6 mt-1">
-                  {story.text}
-                </p>
-              </Link>
-
-              <div className="flex items-center gap-x-4 mt-4">
-                <div className="flex items-center gap-x-2">
-                  <StoryLikeButton
-                    storyId={story.id}
-                    isLikedByUser={story.isLikedByUser}
-                    className="w-5 h-5 text-soft-foreground"
-                  />
-                  <span className="text-sm text-soft-foreground">{story.likesCount}</span>
-                </div>
-
-                <Link href={`/story/${story.id}#comments`} className="flex items-center gap-x-2">
-                  <MessageSquareTextIcon className="w-5 h-5 text-soft-foreground" />
-                  <span className="text-sm text-soft-foreground">{story.commentsCount}</span>
-                </Link>
-
-                <Share2Icon
-                  className="w-5 h-5 text-soft-foreground ms-auto"
-                  onClick={() => share(story.id)}
-                />
-              </div>
-            </div>
-          </div>
-        ))}
-
-        {isFetching && (
+      <InfiniteScroll
+        next={fetchNextPage}
+        dataLength={stories.length}
+        hasMore={isFetching || hasNextPage}
+        loader={
           <>
             {Array(5)
               .fill(0)
@@ -92,8 +53,54 @@ export default function UserStories({ id }: UserStoriesProps) {
                 </div>
               ))}
           </>
-        )}
-      </section>
+        }
+        className="divide-y divide-border"
+      >
+        {stories.map((story) => (
+          <div key={story.id} className="flex gap-x-2 pb-6 not-first:pt-6">
+            <Link href={`/author/${story.user.id}`}>
+              <UserAvatar user={story.user} />
+            </Link>
+
+            <div className="flex-1 mt-1">
+              <div className="flex gap-x-2 items-center">
+                <Link href={`/author/${story.user.id}`}>
+                  <span className="font-bold">{getUserName(story.user)}</span>
+                </Link>
+                <span className="size-1 bg-gray-300 dark:bg-border rounded-sm"></span>
+                <span className="text-[10px] text-light-gray-900 dark:text-soft-foreground">
+                  {formatStoryCreateAt(story.createdAt)}
+                </span>
+              </div>
+
+              <Link href={`/story/${story.id}`}>
+                <p className="w-full text-sm text-soft-foreground whitespace-pre-line line-clamp-6 leading-6 mt-1">
+                  {story.text}
+                </p>
+              </Link>
+
+              <div className="flex items-center gap-x-4 mt-4">
+                <StoryLikeButton
+                  storyId={story.id}
+                  likesCount={story.likesCount}
+                  isLikedByUser={story.isLikedByUser}
+                  className="w-5 h-5 text-soft-foreground"
+                />
+
+                <Link href={`/story/${story.id}#comments`} className="flex items-center gap-x-2">
+                  <MessageSquareTextIcon className="w-5 h-5 text-soft-foreground" />
+                  <span className="text-sm text-soft-foreground">{story.commentsCount}</span>
+                </Link>
+
+                <Share2Icon
+                  className="w-5 h-5 text-soft-foreground ms-auto"
+                  onClick={() => share(story.id)}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </InfiniteScroll>
 
       {isShareSheetOpen && (
         <ShareStorySheet
