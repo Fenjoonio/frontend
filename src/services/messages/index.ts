b49974c30d1a681ae10de0cdefbb8c023a463bd5
Yasin Silavi <ys.silavi@gmail.com>
2025-05-11 16:25:24 +0330

    feat: implement messages service

---
+++ src/services/messages/index.ts
@@ -0,0 +1,4 @@
+export * from "./hooks";
+export * from "./constants";
+export * from "./functions";
+export type { Message } from "./types";
---
+++ src/services/messages/types.ts
@@ -0,0 +1,41 @@
+export type Message = {
+  id: number;
+  fromId: number;
+  message: string;
+  createdAt: string;
+  readAt?: string;
+  isUpdated: boolean;
+};
+
+export type SendMessageBody = {
+  to: number;
+  message: string;
+};
+
+export type GetChatMessagesParams = {
+  userId: number;
+  limit?: number;
+  cursor?: string;
+};
+
+export type GetChatMessagesResponse = {
+  pagination: {
+    limit: number;
+    total: number;
+    cursor: string;
+  };
+  messages: Message[];
+};
+
+export type UpdateMessageBody = {
+  id: number;
+  message: string;
+};
+
+export type DeleteMessageParams = {
+  id: number;
+};
+
+export type ReadMessageParams = {
+  id: number;
+};
