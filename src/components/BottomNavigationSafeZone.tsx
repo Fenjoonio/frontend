b3a4475d00ef3374a774a226c3f560164f8859cd
Yasin Silavi <ys.silavi@gmail.com>
2025-05-23 13:03:50 +0330

    fix: hide bottom-navigation only on messages/[id] page

--- src/components/BottomNavigationSafeZone.tsx
+++ src/components/BottomNavigationSafeZone.tsx
@@ -7,7 +7,7 @@ import BottomNavigation from "@/components/BottomNavigation";
 export default function BottomNavigationSafeZone({ children }: PropsWithChildren) {
   const pathname = usePathname();
 
-  const hidePages: string[] = ["story", "messages"];
+  const hidePages: string[] = ["story", "messages/"];
   const isHidden = hidePages.some((page) => pathname.includes(page));
 
   if (isHidden) {
