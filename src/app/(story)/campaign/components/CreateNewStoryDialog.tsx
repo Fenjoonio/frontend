3e628048109fd81a1312c80d80025c9493c386af
Yasin Silavi <ys.silavi@gmail.com>
2025-05-28 10:30:26 +0330

    feat: implement campaign page

---
+++ src/app/(story)/campaign/components/CreateNewStoryDialog.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { cn } from "@/lib/utils/classnames";
+import { SparklesIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { sendGAEvent } from "@next/third-parties/google";
+import { useWriteStoryWithAi } from "@/services/stories";
+import { randomNumberBetween } from "@/lib/utils/numbers";
+import { useAddCampaignStory } from "@/services/campaigns";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const placeholders = [
+  `هر روز همون میز، همون صندلی، همون قهوه.
+فقط اون دیگه نمیاد.`,
+  `قهوه‌اش سرد شده بود، نگاهش هنوز داغ.
+گفتم: منتظر کی بودی؟
+گفت: یکی که قول نداده بود، ولی همیشه می‌اومد`,
+  `بارون گرفته بود و همه دنبال سقف بودن.
+اون ولی، دست‌هاش رو باز کرده بود!
+«اگه قرار خیس بشم، بذار کامل خیس شم…»`,
+];
+
+type CreateNewStoryDialogProps = {
+  open: boolean;
+  onOpenChange: any;
+};
+
+export default function CreateNewStoryDialog({ open, onOpenChange }: CreateNewStoryDialogProps) {
+  const [text, setText] = useState("");
+  const { mutate: createStory, isPending: isCreateStoryPending } = useAddCampaignStory({
+    onSuccess: () => {
+      setText("");
+      onOpenChange(false);
+      sendGAEvent("event", "campaign_story_created", {});
+    },
+  });
+
+  const { mutate: writeStoryWithAi, isPending: isWriteWithAiPending } = useWriteStoryWithAi({
+    onSuccess: (story) => {
+      setText((prevState) => `${prevState.trim()} ${story}`);
+      sendGAEvent("event", "write_story_with_ai", {});
+    },
+    onError: () => {
+      toast.error("این قابلیت در حال حاضر در دسترس نیست");
+      sendGAEvent("event", "write_story_with_ai_error", {});
+    },
+  });
+
+  const createNewStory = () => {
+    createStory({ text: text.trim() });
+  };
+
+  const getHelpFromAi = () => {
+    if (text.length < 5) {
+      toast.info("برای شروع چند کلمه بنویس");
+      return;
+    }
+
+    writeStoryWithAi({ text });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="h-svh">
+        <DialogHeader>
+          <DialogTitle>داستان جدید</DialogTitle>
+          <DialogDescription>
+            هر لحظه‌ای می‌تونه یه داستان داشته باشه؛ به اطرافت نگاه کن و داستانت رو بنویس. (از{" "}
+            <span className="text-foreground">۲۵ تا ۲۵۰</span> حرف)
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="relative mt-6">
+          <Textarea
+            value={text}
+            maxLength={250}
+            disabled={isWriteWithAiPending}
+            placeholder={placeholders[randomNumberBetween(0, placeholders.length - 1)]}
+            className="w-full min-h-40 p-4 resize-none border border-border rounded-lg"
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          <div
+            className={cn(
+              "slide-out flex items-center justify-end absolute bottom-5 left-3 overflow-hidden bg-soft-background rounded-md",
+              { "opacity-50": isWriteWithAiPending }
+            )}
+            onClick={getHelpFromAi}
+          >
+            <span className="h-10 flex items-center text-nowrap px-2.5 -me-2.5">
+              تکمیل با هوش مصنوعی
+            </span>
+
+            <div className="relative z-10 p-2.5">
+              <SparklesIcon className="size-5 text-primary" />
+            </div>
+          </div>
+        </div>
+
+        <span className="text-sm text-soft-foreground mt-2">نکته: داستان‌هایی که برای مسابقه می‌نویسید در صفحه اصلی فنجون نیز نمایش داده خواهند شد.</span>
+
+        <DialogFooter className="flex gap-x-2">
+          <DialogClose asChild>
+            <Button variant="outline" className="basis-24">
+              بستن
+            </Button>
+          </DialogClose>
+
+          <Button
+            type="submit"
+            disabled={isCreateStoryPending || isWriteWithAiPending}
+            className="flex-1"
+            onClick={createNewStory}
+          >
+            ثبت داستان
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ src/app/(story)/campaign/page.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { PenIcon } from "lucide-react";
+import { useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import BackArrow from "@/components/BackArrow";
+import { getUserName } from "@/lib/utils/users";
+import PullToRefresh from "@/components/PullToRefresh";
+import { sendGAEvent } from "@next/third-parties/google";
+import { useAuthContext } from "@/providers/AuthProvider";
+import InfiniteScroll from "react-infinite-scroll-component";
+import CreateNewStoryDialog from "./components/CreateNewStoryDialog";
+import Story, { StorySkeleton } from "@/app/(story)/components/Story";
+import { useGetCampaignLeaderboard, useGetCampaignStories } from "@/services/campaigns";
+import UserAvatar from "@/components/UserAvatar";
+
+export default function HomePage() {
+  const router = useRouter();
+  const { isLoggedIn } = useAuthContext();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data, refetch, fetchNextPage, hasNextPage, isFetching } = useGetCampaignStories({
+    page: 1,
+    limit: 10,
+  });
+
+  const stories = useMemo(() => {
+    return data?.pages ? data.pages.flatMap((page) => page.stories ?? []) : [];
+  }, [data?.pages]);
+
+  const { data: leaderboard, isFetching: isLeaderboardFetching } = useGetCampaignLeaderboard();
+
+  const openAddStoryModalIfLoggedIn = () => {
+    if (!isLoggedIn) {
+      router.push("/accounts/login?redirect=/");
+      return;
+    }
+
+    setIsModalOpen(true);
+    sendGAEvent("add_story", "click", { location: "home" });
+  };
+
+  const onRefresh = async () => {
+    await refetch();
+  };
+
+  return (
+    <div className="pb-4">
+      <header
+        style={{ paddingTop: "calc(env(safe-area-inset-top) + 12px)" }}
+        className="w-[calc(100%-1px)] flex items-center sticky top-0 right-0 z-10 bg-background border-b border-border pb-3 px-2"
+      >
+        <BackArrow />
+        <h1 className="text-lg font-bold mt-1">مسابقه یک فنجون داستان</h1>
+      </header>
+
+      <div className="px-5 mt-4">
+        <span className="block text-sm text-soft-foreground mt-1">
+          به اولین مسابقه داستان نویسی فنجون خوش اومدید!
+        </span>
+      </div>
+
+      <h3 className="text-sm text-soft-foreground mt-6 px-5">قوانین مسابقه</h3>
+
+      <ol className="flex gap-y-1 flex-col mt-4 px-5">
+        <li>۱. تمامی داستان‌ها باید بین ۲۵ تا ۲۵۰ حرف باشند.</li>
+        <li>
+          ۲. داستان‌ها باید با موضوع اصلی عشق یا موضوعات انتقام، نفرت و صداقت نوشته شوند. داستان‌های
+          نامربوط از صفحه مسابقه حذف می‌شوند.
+        </li>
+      </ol>
+
+      <h3 className="text-sm text-soft-foreground mt-6 px-5">نفرات برتر</h3>
+
+      <div className="px-5 mt-4">
+        {isLeaderboardFetching
+          ? Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <div key={index} className="flex gap-x-2 pb-2 not-first:pt-2">
+                  <div>
+                    <div className="size-11 bg-gray-300 dark:bg-border opacity-40 rounded-lg animate-pulse"></div>
+                  </div>
+
+                  <div className="flex-1 mt-1">
+                    <div className="w-20 h-4 bg-gray-300 dark:bg-border opacity-40 rounded-full animate-pulse"></div>
+                    <div className="w-10 h-4 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-1"></div>
+                  </div>
+                </div>
+              ))
+          : leaderboard?.map((item) => (
+              <div key={item.user.id} className="flex items-center justify-between">
+                <div className="flex items-center gap-x-2 pb-4 not-first:pt-4">
+                  <UserAvatar user={item.user} className="size-11" />
+                  <div className="flex gap-y-1 flex-col justify-center">
+                    <span className="text-sm font-bold">{getUserName(item.user)}</span>
+                    <span className="text-xs text-soft-foreground">{item.totalLikes} لایک</span>
+                  </div>
+                </div>
+
+                <span>{item.rank}</span>
+              </div>
+            ))}
+      </div>
+
+      <h3 className="text-sm text-soft-foreground mt-6 px-5">لیست جوایز</h3>
+
+      <ol className="flex gap-y-1 flex-col mt-4 px-5">
+        <li>نفر اول: جایزه نقدی + کتاب +‌ اکانت حرفه‌ای فنجون</li>
+        <li>نفر دوم: کتاب + اکانت حرفه‌ای فنجون</li>
+        <li>نفر نفر سوم: اکانت حرفه‌ای فنجون</li>
+      </ol>
+
+      <h3 className="text-sm text-soft-foreground mt-6 px-5">آخرین داستان‌ها</h3>
+
+      <PullToRefresh onRefresh={onRefresh} className="mx-5 mt-4">
+        <InfiniteScroll
+          next={fetchNextPage}
+          dataLength={stories.length}
+          hasMore={isFetching || hasNextPage}
+          loader={
+            <>
+              {Array(5)
+                .fill(0)
+                .map((_, index) => (
+                  <StorySkeleton key={index} className="pb-6 not-first:pt-6" />
+                ))}
+            </>
+          }
+          className="divide-y divide-border"
+        >
+          {stories.map((story) => (
+            <Story key={story.id} story={story} className="flex gap-x-2 pb-6 not-first:pt-6" />
+          ))}
+        </InfiniteScroll>
+      </PullToRefresh>
+
+      <CreateNewStoryDialog open={isModalOpen} onOpenChange={setIsModalOpen} />
+
+      <button
+        style={{ bottom: "calc(env(safe-area-inset-bottom) + 80px)" }}
+        className="fixed left-4 p-4 bg-primary text-light-gray-100 rounded-lg cursor-pointer"
+        onClick={openAddStoryModalIfLoggedIn}
+      >
+        <PenIcon className="size-5" />
+      </button>
+    </div>
+  );
+}
