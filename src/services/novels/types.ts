e01604bf3dd53c80d77b9fca259eab780874c8ae
Yasin Silavi <ys.silavi@gmail.com>
2025-07-01 00:53:44 +0330

    refactor: move mock data to services folder

--- src/services/novels/types.ts
+++ src/services/novels/types.ts
@@ -1,4 +1,35 @@
+import type { User } from "@/services/user";
+
 export type Chapter = {
   id: number;
   title: string;
+};
+
+export type Novel = {
+  id: number;
+  chapters: Chapter[];
+  user: User;
+};
+
+export type GetNovelsParams = {
+  page: number;
+  limit: number;
+};
+
+export type GetNovelsResponse = {
+  pagination: {
+    limit: number;
+    page: number;
+    pages: number;
+    total: number;
+  };
+  novels: Novel[];
+};
+
+export type GetNovelByIdParams = {
+  id: number;
+};
+
+export type GetNovelChaptersParams = {
+  id: number;
 };
