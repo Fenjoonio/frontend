498691d5b087dbe5c42abf0b1099d5fed2b2f26a
ms8ziaee <ms8ziaee@gmail.com>
2025-07-01 18:15:48 +0330

    fix: pagination story likes sheet

--- src/app/(story)/components/StoryLikersSheet.tsx
+++ src/app/(story)/components/StoryLikersSheet.tsx
@@ -9,7 +9,7 @@ import {
   SheetContent,
   SheetDescription,
   SheetHeader,
-  SheetTitle,
+  SheetTitle
 } from "@/components/ui/sheet";
 import { useMemo } from "react";
 import Link from "next/link";
@@ -38,42 +38,51 @@ export default function StoryLikersSheet({ storyId, isOpen, onOpenChange }: Stor
           <SheetTitle>لایک‌ها</SheetTitle>
           <SheetDescription>لیست کسانی که داستان شما رو پسندیدن</SheetDescription>
         </SheetHeader>
-
-        <InfiniteScroll
-          next={fetchNextPage}
-          dataLength={users.length}
-          hasMore={isFetching || hasNextPage}
-          loader={
-            <>
-              {Array(5)
-                .fill(0)
-                .map((_, index) => (
-                  <div key={index} className="flex items-center gap-x-2 pb-6 not-first:pt-6">
-                    <div>
-                      <div className="size-7 bg-gray-300 dark:bg-border opacity-40 rounded-lg animate-pulse"></div>
-                    </div>
-                    <div className="flex-1">
-                      <div className="w-20 h-4 bg-gray-300 dark:bg-border opacity-40 rounded-full animate-pulse"></div>
-                    </div>
-                  </div>
-                ))}
-            </>
-          }
-          className="divide-y divide-border"
+        <div
+          id="scrollableDiv"
+          className="overflow-y-auto scrollbar-none"
+          style={{ height: "300px", padding: "1px" }}
         >
-          {users.map((user) => (
-            <Link
-              key={user.id}
-              href={`/author/${user.id}`}
-              className="flex gap-x-2 items-center gap-2 py-4"
-            >
-              <UserAvatar user={user} />
-              <span>{getUserName(user)}</span>
-
-              <ArrowLeftIcon className="size-5 ms-auto" />
-            </Link>
-          ))}
-        </InfiniteScroll>
+          <InfiniteScroll
+            next={fetchNextPage}
+            dataLength={users.length}
+            hasMore={isFetching || hasNextPage}
+            scrollableTarget="scrollableDiv"
+            loader={
+              <>
+                {Array(5)
+                  .fill(0)
+                  .map((_, index) => (
+                    <div
+                      key={index}
+                      className="flex items-center gap-x-2 pb-6 not-first:pt-6"
+                    >
+                      <div>
+                        <div className="size-7 bg-gray-300 dark:bg-border opacity-40 rounded-lg animate-pulse"></div>
+                      </div>
+                      <div className="flex-1">
+                        <div
+                          className="w-20 h-4 bg-gray-300 dark:bg-border opacity-40 rounded-full animate-pulse"></div>
+                      </div>
+                    </div>
+                  ))}
+              </>
+            }
+            className="divide-y divide-border"
+          >
+            {users.map((user) => (
+              <Link
+                key={user.id}
+                href={`/author/${user.id}`}
+                className="flex gap-x-2 items-center gap-2 py-4"
+              >
+                <UserAvatar user={user} />
+                <span>{getUserName(user)}</span>
+                <ArrowLeftIcon className="size-5 ms-auto" />
+              </Link>
+            ))}
+          </InfiniteScroll>
+        </div>
       </SheetContent>
     </Sheet>
   );
