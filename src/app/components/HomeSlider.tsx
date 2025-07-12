0d8ed68b40f0282923f9805fc3c5c0b8bbf966ec
Yasin Silavi <ys.silavi@gmail.com>
2025-07-12 15:28:08 +0330

    feat: replace old slides with new one

--- src/app/components/HomeSlider.tsx
+++ src/app/components/HomeSlider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import SwiperJS from "swiper";
 import { Autoplay } from "swiper/modules";
@@ -16,31 +17,23 @@ export default function HomeSlider({ className }: HomeSliderProps) {
     <Swiper autoplay={{ delay: 10_000 }} loop className={className}>
       <SwiperSlide className="px-5">
         <Link
-          href="https://instagram.com/fenjoon.io"
-          className="w-full h-48 flex flex-col justify-center items-center bg-primary rounded-sm px-4"
+          href="/novels/3"
+          className="w-full h-48 flex items-center justify-center rounded-sm overflow-hidden relative px-4"
         >
-          <span className="text-center text-xl text-light-gray-100 font-black">
-            فنجون رو دنبال کنید!
-          </span>
-          <span className="text-center text-sm text-light-gray-100 mt-1">
-            صفحه اینستاگرام فنجون ساخته شده، یادتون نره دنبالمون کنید!
-          </span>
-          <span className="bg-white text-primary rounded-sm py-1 px-6 mt-4">دنبال کردن</span>
-        </Link>
-      </SwiperSlide>
+          <Image
+            src="https://s3.lexoya.com/fenjoon/banners/t5125425-f45f-41ec-a143-915faed27823.png"
+            alt="تصویر داستان"
+            fill
+            className="object-cover blur-xs"
+          />
 
-      <SwiperSlide className="px-5">
-        <Link
-          href="/thread"
-          className="w-full h-48 flex flex-col justify-center items-center bg-success rounded-sm px-4"
-        >
-          <span className="text-xl text-light-gray-100 font-black">
-            شروع به نوشتن کرد ... اما مردد بود
-          </span>
-          <span className="text-sm text-light-gray-100 mt-1">
-            دوست داری بعدش چی بشه؟ بقیه‌ش رو تو بنویس!
-          </span>
-          <span className="bg-white text-success rounded-sm py-1 px-6 mt-4">بزن بریم!</span>
+          <div className="flex flex-col justify-center items-center relative z-10">
+            <span className="text-center text-xl text-light-gray-100 font-black">سبزپوشی خفته</span>
+            <span className="text-center text-sm text-light-gray-100 mt-1">
+              قصه دختری که میان سکوت، صدای مردم شد...
+            </span>
+            <span className="bg-white text-primary rounded-sm py-1 px-6 mt-4">خواندن</span>
+          </div>
         </Link>
       </SwiperSlide>
     </Swiper>
