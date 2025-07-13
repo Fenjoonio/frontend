a82a741e6746fd07d08b3754aca989af837feea4
Yasin Silavi <ys.silavi@gmail.com>
2025-07-13 19:56:01 +0330

    feat: add new novel banner

--- src/app/components/HomeSlider.tsx
+++ src/app/components/HomeSlider.tsx
@@ -36,6 +36,28 @@ export default function HomeSlider({ className }: HomeSliderProps) {
           </div>
         </Link>
       </SwiperSlide>
+
+      <SwiperSlide className="px-5">
+        <Link
+          href="/novel/3"
+          className="w-full h-48 flex items-center justify-center rounded-sm overflow-hidden relative px-4"
+        >
+          <Image
+            src="https://s3.lexoya.com/fenjoon/banners/a4335425-d15f-41fc-a143-915faed27912.png"
+            alt="تصویر داستان"
+            fill
+            className="object-cover blur-xs"
+          />
+
+          <div className="flex flex-col justify-center items-center relative z-10">
+            <span className="text-center text-xl text-light-gray-100 font-black">دالیای سیاه</span>
+            <span className="text-center text-sm text-light-gray-100 mt-1">
+              او تنها یک قاتل نبود، دالیای سیاه بود؛ پژمرده اما مرگ‌بار.
+            </span>
+            <span className="bg-white text-primary rounded-sm py-1 px-6 mt-4">خواندن</span>
+          </div>
+        </Link>
+      </SwiperSlide>
     </Swiper>
   );
 }
