0be2b158fff52749c010c7a9398a353e067cd669
Yasin Silavi <ys.silavi@gmail.com>
2025-07-16 23:53:59 +0330

    feat: move editor-toolkit to a separate component

--- src/app/(story)/novel/new/page.tsx
+++ src/app/(story)/novel/new/page.tsx
@@ -1,33 +1,18 @@
 "use client";
 
-import Editor from "./components/Editor";
+import dynamic from "next/dynamic";
+import { SaveIcon } from "lucide-react";
+import { type KeyboardEvent } from "react";
 import { Input } from "@/components/ui/input";
 import BackArrow from "@/components/BackArrow";
-import { type KeyboardEvent, useRef } from "react";
-import { type Editor as TipTapEditor } from "@tiptap/core";
-import {
-  AlignCenterIcon,
-  AlignJustifyIcon,
-  AlignLeftIcon,
-  AlignRightIcon,
-  BoldIcon,
-  ItalicIcon,
-  MinusIcon,
-  QuoteIcon,
-  RedoIcon,
-  SaveIcon,
-  TypeIcon,
-  UnderlineIcon,
-  UndoIcon,
-} from "lucide-react";
-import { Popover, PopoverArrow, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+
+const Editor = dynamic(() => import("./components/Editor"), { ssr: false });
 
 export default function NewNovelPage() {
-  const editorRef = useRef<TipTapEditor | null>(null);
-
   const onTitleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.code === "Enter") {
-      console.log("e.code");
+      // console.log("e.code");
     }
   };
 
@@ -44,86 +29,16 @@ export default function NewNovelPage() {
       <article className="p-5">
         <Input
           placeholder="عنوان این بخش"
-          className="border-none !text-xl placeholder:text-gray-300 placeholder:text-xl font-bold p-0"
+          className="border-none shadow-none !text-xl placeholder:text-gray-300 placeholder:text-xl font-bold p-0"
           onKeyDown={onTitleEnter}
         />
 
-        <Editor className="pt-5" onEditorCreate={(editor) => (editorRef.current = editor)} />
-
-        <div className="flex fixed bottom-5 start-5 bg-soft-background rounded-lg">
-          <div className="flex gap-x-1 items-center bg-background rounded-md my-1 ms-1">
-            <div className="p-2">
-              <QuoteIcon />
-            </div>
-
-            <Popover>
-              <PopoverTrigger className="p-2">
-                <TypeIcon />
-              </PopoverTrigger>
-              <PopoverContent className="p-1">
-                <div className="flex bg-background rounded-md">
-                  <div className="p-2">
-                    <UnderlineIcon />
-                  </div>
-
-                  <div className="p-2">
-                    <ItalicIcon />
-                  </div>
-
-                  <div className="p-2">
-                    <BoldIcon />
-                  </div>
-                </div>
-
-                <PopoverArrow className="mb-1" />
-              </PopoverContent>
-            </Popover>
-
-            <Popover>
-              <PopoverTrigger className="p-2">
-                <AlignJustifyIcon />
-              </PopoverTrigger>
-              <PopoverContent className="p-1">
-                <div className="flex bg-background rounded-md">
-                  <div className="p-2">
-                    <AlignJustifyIcon />
-                  </div>
-
-                  <div className="p-2">
-                    <AlignRightIcon />
-                  </div>
-
-                  <div className="p-2">
-                    <AlignCenterIcon />
-                  </div>
-
-                  <div className="p-2">
-                    <AlignLeftIcon />
-                  </div>
-                </div>
-
-                <PopoverArrow className="mb-1" />
-              </PopoverContent>
-            </Popover>
-
-            <div className="p-2">
-              <RedoIcon />
-            </div>
-
-            <div className="p-2">
-              <UndoIcon />
-            </div>
-          </div>
-
-          <div className="p-3">
-            <MinusIcon />
-          </div>
-        </div>
+        <Editor className="mt-5" toolkitClassName="fixed bottom-5 start-5" />
 
         <div className="flex fixed bottom-5 end-5 bg-primary rounded-lg">
-          <div className="p-3">
+          <Button size="sm" className="w-12">
             <SaveIcon />
-          </div>
+          </Button>
         </div>
       </article>
     </section>
