e10a1d2cd2fbc76c379c35a7edf67ef72b6de005
Yasin Silavi <ys.silavi@gmail.com>
2025-07-31 18:02:41 +0330

    refactor: get chapter data from the get-chapter-by-id api

--- src/app/(story)/novel/[id]/chapter/[chapterId]/page.tsx
+++ src/app/(story)/novel/[id]/chapter/[chapterId]/page.tsx
@@ -1,37 +1,81 @@
-import Link from "next/link";
-import { notFound } from "next/navigation";
-import Viewer from "./components/NovelViewer";
-import { Button } from "@/components/ui/button";
-import { getNovelChapters } from "@/services/novels";
+"use client";
 
-type NovelChapterPageProps = {
-  params: Promise<{ id: string; chapterId: string }>;
+import Link from "next/link";
+import { cn } from "@/lib/utils/classnames";
+import Reader from "@/components/ReaderNew";
+import BackArrow from "@/components/BackArrow";
+import { Button } from "@/components/ui/button";
+import { useGetChapterById } from "@/services/novels";
+import { notFound, useParams } from "next/navigation";
+
+type ChapterSkeletonProps = {
+  className?: string;
 };
 
-export default async function NovelChapterPage({ params }: NovelChapterPageProps) {
-  const { id, chapterId } = await params;
+function ChapterSkeleton({ className }: ChapterSkeletonProps) {
+  return (
+    <div className={cn("p-5", className)}>
+      <div className="w-[90%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse"></div>
+      <div className="w-[60%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-3"></div>
+      <div className="w-[100%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-3"></div>
+      <div className="w-[80%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-3"></div>
 
-  const chapters = await getNovelChapters({ id: Number(id) });
+      <div className="w-[40%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-10"></div>
+      <div className="w-[80%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-3"></div>
+      <div className="w-full h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-3"></div>
+      <div className="w-[30%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse mt-3"></div>
+    </div>
+  );
+}
 
-  const chapterContent = chapters[+chapterId]?.content;
-  const hasNextChapter = !!chapters[+chapterId + 1]?.content;
+export default function NovelChapterPage() {
+  const params = useParams<{ id: string; chapterId: string }>();
+  const { data, isFetched } = useGetChapterById({ id: +params.id, chapterId: +params.chapterId });
 
-  if (!id || !chapterId || Number.isNaN(chapterId) || !chapterContent) {
+  if (!data) {
+    return (
+      <>
+        <header
+          style={{ paddingTop: "calc(env(safe-area-inset-top) + 12px)" }}
+          className="w-[calc(100%-1px)] flex items-center sticky top-0 right-0 z-10 bg-background border-b border-border pb-3 px-2"
+        >
+          <BackArrow />
+          <div className="w-[40%] h-5 bg-gray-300 dark:bg-border opacity-20 rounded-full animate-pulse"></div>
+        </header>
+
+        <ChapterSkeleton />
+      </>
+    );
+  }
+
+  if (isFetched && !data) {
     notFound();
   }
 
   return (
     <section className="min-h-svh">
-      <Viewer content={chapterContent} className="py-4 px-5" />
+      <header
+        style={{ paddingTop: "calc(env(safe-area-inset-top) + 12px)" }}
+        className="w-[calc(100%-1px)] flex items-center sticky top-0 right-0 z-10 bg-background border-b border-border pb-3 px-2"
+      >
+        <BackArrow />
+        <h1 className="text-lg font-bold mt-1">{data?.chapter.title || ""}</h1>
+      </header>
 
-      {hasNextChapter ? (
-        <Link href={`/novel/${id}/chapter/${+chapterId + 1}`} replace className="block py-8 px-5">
+      <Reader initialState={data?.chapter.jsonContent || ""} className="py-4 px-5" />
+
+      {data?.hasNext ? (
+        <Link
+          href={`/novel/${params.id}/chapter/${+params.chapterId + 1}`}
+          replace
+          className="block py-8 px-5"
+        >
           <Button variant="outline" className="w-full">
             بخش بعدی
           </Button>
         </Link>
       ) : (
-        <Link href={`/novel/${id}`} replace className="block py-8 px-5">
+        <Link href={`/novel/${params.id}`} replace className="block py-8 px-5">
           <Button variant="outline" className="w-full">
             بازگشت به صفحه داستان
           </Button>
