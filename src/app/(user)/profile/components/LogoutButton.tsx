bc30e2ebd400c2dced2c86445c6ca6c6c0f7314a
Yasin Silavi <ys.silavi@gmail.com>
2025-08-02 19:35:16 +0330

    refactor: use new use-logout hook

--- src/app/(user)/profile/components/LogoutButton.tsx
+++ src/app/(user)/profile/components/LogoutButton.tsx
@@ -1,28 +1,34 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { logout } from "@/services/accounts";
+import { useLogout } from "@/services/accounts";
+import { useQueryClient } from "@tanstack/react-query";
 import { ArrowLeftIcon, LogOutIcon } from "lucide-react";
 import { useAuthContext } from "@/providers/AuthProvider";
 import { getUserCredentials } from "@/app/(user)/accounts/actions";
 
 export default function LogoutButton() {
   const router = useRouter();
+  const queryClient = useQueryClient();
   const { setIsLoggedIn } = useAuthContext();
+  const { mutate: logout } = useLogout({
+    onSuccess: () => {
+      router.push("/");
+      queryClient.clear();
+      setIsLoggedIn(false);
+    },
+  });
 
   const logoutAndRedirect = async () => {
     const { refreshToken } = await getUserCredentials();
-    await logout({ refreshToken });
-
-    setIsLoggedIn(false);
-
-    setTimeout(() => {
-      router.push("/");
-    }, 0);
+    logout({ refreshToken });
   };
 
   return (
-    <button className="w-full flex gap-x-2 items-center py-3 px-2" onClick={logoutAndRedirect}>
+    <button
+      className="w-full flex gap-x-2 items-center cursor-pointer py-3 px-2"
+      onClick={logoutAndRedirect}
+    >
       <div className="bg-background rounded-[8px] p-2">
         <LogOutIcon className="w-5 h-5 " />
       </div>
