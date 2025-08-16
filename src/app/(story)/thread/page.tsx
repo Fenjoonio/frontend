17b358c8e34eb84bfe5daaa7a80bfe11786423d1
Yasin Silavi <ys.silavi@gmail.com>
2025-08-17 00:15:06 +0330

    chore: change thread story

--- src/app/(story)/thread/page.tsx
+++ src/app/(story)/thread/page.tsx
@@ -18,7 +18,7 @@ import ShareStorySheet from "@/app/(story)/components/ShareStorySheet";
 import { MessageSquareTextIcon, PenIcon, Share2Icon } from "lucide-react";
 import { useGetSingleStory, useGetInfiniteStoryComments } from "@/services/stories";
 
-const STORY_ID = 457;
+const STORY_ID = 530;
 
 export default function ThreadPage() {
   const router = useRouter();
